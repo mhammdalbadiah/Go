515b51c12418ad44ab72d242ff560ffd1265bcfb
m7md494 <mhammdqqpp@gmail.com>
2026-01-30 00:39:59 +0300

    Add Functions 'stv' && 'rff'

--- Lab_9.go
+++ Lab_9.go
@@ -8,6 +8,40 @@ import (
 	"os"
 )
 
+var courses int
+
+// rff : Read From File
+func rff() error {
+
+	file, err := os.Open("Data.bin")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	dec := gob.NewDecoder(file)
+
+	var hours []float32
+	var grades []float32
+
+	if err := dec.Decode(&hours); err != nil {
+		return err
+	}
+
+	if err := dec.Decode(&grades); err != nil {
+		return err
+	}
+
+	fmt.Println("Data From The File:")
+	for i := 0; i < len(hours); i++ {
+		fmt.Printf("Course %d -> Hours: %.1f | Grade: %.1f\n",
+			i+1, hours[i], grades[i])
+	}
+
+	return nil
+}
+
+// stv : Save to File
 func stv(hours []float32, Grades []float32) error {
 
 	file, err := os.OpenFile("Data.bin", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
@@ -30,14 +64,17 @@ func stv(hours []float32, Grades []float32) error {
 
 }
 
+// Cal_GPA : Calculate GPA
 func Cal_GPA() float32 {
 
-	courses := 0
+	cou := 0
 	var GPA float32
 	var SumG float32 = 0.0
 	var SumH float32 = 0.0
 	fmt.Println("Please Enter The Number Of Courses you have : ")
-	fmt.Scan(&courses)
+	fmt.Scan(&cou)
+	courses = cou
+
 	hours := make([]float32, courses)
 	Grades := make([]float32, courses)
 
@@ -50,12 +87,17 @@ func Cal_GPA() float32 {
 
 	}
 
+	GPA = 0
 	for i := 0; i < courses; i++ {
-		SumG = SumG + Grades[i]
-		SumH = SumH + hours[i]
+		GPA += Grades[i] * hours[i]
 	}
+	GPA = GPA / SumH
 
-	GPA = (SumG + SumH) / SumG
+	if err := stv(hours, Grades); err != nil {
+		fmt.Println("Error storing data:", err)
+	} else {
+		fmt.Println("Data stored successfully")
+	}
 
 	return GPA
 
@@ -69,12 +111,14 @@ func main() {
 	fmt.Println(" 2)  To Print Your Schedule")
 	fmt.Println(" 3)  To Add Your Schedule ")
 	fmt.Println(" 4)  To Exit 				")
+	fmt.Println("======================")
+	fmt.Printf(":")
 	fmt.Scan(&choice)
 
 	switch choice {
 
 	case 1:
-
+		fmt.Println(Cal_GPA())
 	case 2:
 
 	case 3:
